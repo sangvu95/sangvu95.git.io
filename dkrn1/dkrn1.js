0fc6eca2a8cef0785fcb00d23fd9806be746d927
sangvu95 <43396144+sangvu95@users.noreply.github.com>
2018-10-11 10:01:36 +0700

    Add files via upload

---
+++ dkrn1/dkrn1.js
@@ -0,0 +1,36 @@
+//Bài 1:
+function bai1(a, b) {
+    if (typeof a == 'number' && typeof b == 'number') {
+        document.write(
+            a + ", " + b + " là số" +"</br>");
+        document.write (Math.max(a, b)+" là số lớn nhất");
+
+    }
+}
+//testcase bai1(6,7)
+
+//Bài 2:
+function bai2(n) {var a = 1;
+    if (n > 0) {
+        for (i = n; i > 1; i--) { a = a * i }
+        document.write(a+"</br>")
+    }
+}
+//testcase bai2(3) 
+
+//Bài 3:
+function bai3(arr) {var newArr = [];
+    for (i = 0; i < arr.length; i++) { if (arr[i] % 2 == 0) { newArr.push(arr[i]) } }
+    return newArr.sort(function(a, b) { return b - a });
+}
+//testcase  bai3([21,14,2,5,10])
+
+//Bài 4:
+
+function bai4(a,b,c){var d;
+    var user={username:a,password:b,confirm: c};
+    if(a.length>=1&&a.length<=20&&b.length>=6&&b.length<=32&&c==b)
+    {d=document.write("OK")}
+    return d}
+//testcase bai4("id","matkhau","matkhau")
+//testcase bai4("","matkhau","matkhau")
